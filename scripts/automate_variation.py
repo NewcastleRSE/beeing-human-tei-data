e7c3a467a45e6e6a90cd5724afb2ffd17d43a698
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2023-10-24 12:07:15 +0100

    method works but is a little too fragile with finding the correct text in 1609 -- change it to use xml traversal

--- scripts/automate_variation.py
+++ scripts/automate_variation.py
@@ -1,6 +1,61 @@
+def getText(string):
+    import re
+
+    # get two targets
+    start_end = string.split()
+
+    # get file name
+    fileName = start_end[0].split('#')[0]
+
+    # isolate xml:id and build full anchor
+    for i, id in enumerate(start_end):
+        start_end[i] = f'<anchor xml:id=\"{id.split("#")[1]}\"/>'
+
+
+    # open file
+    with open(fileName, 'r') as file:
+        text = file.read()
+        # NEED TO REMOVE ANY POSSIBLE EMPTY SPACES BETWEEN THE END OF THE XML:ID AND CLOSING THE TAG
+        # ALTERNATIVELY FIND A WAY OF EXTRACTING TEXT BEWEEN TWO TAGS
+        # find substring
+        result = re.search(f'{start_end[0]}(.*){start_end[1]}', text).group(1)
+        print(result)
+
+
+    return result
+    
+
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('./1623.xml')
+ns = {
+    'TEI': 'http://www.tei-c.org/ns/1.0',
+    'xml': 'http://www.w3.org/XML/1998/namespace'
+}
+
+# Load the document
+tree = ET.parse('./1623_example.xml')
 root = tree.getroot()
 
-print(root);
+
+# Find elements
+# selects the parent (..) of all ptr inside rdg (i.e., selects rdg)
+rdgs = tree.findall("//TEI:rdg/TEI:ptr/..", ns)
+
+for rdg in rdgs:
+    # Find target
+    ptr = rdg.find('TEI:ptr', ns)
+
+    # Record target information in the rdg
+    target = ptr.attrib['target']
+    rdg.attrib['source'] = target
+
+    # Get text from 1609
+    newText = getText(target)
+
+    # Replace ptr with text from 1609
+    rdg.remove(ptr)
+    rdg.text = newText
+
+# To write results
+ET.register_namespace('', 'http://www.tei-c.org/ns/1.0')
+tree.write("test_output.xml")
