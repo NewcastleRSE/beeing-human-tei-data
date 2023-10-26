e2df75959280b7645495e4dbff9e5b31e791a9ba
Tiago Sousa Garcia <tiagosousagarcia@gmail.com>
2023-10-26 10:02:39 +0100

    currently working, though inner elements from 1609 are not preserved

--- scripts/automate_variation.py
+++ scripts/automate_variation.py
@@ -1,23 +1,38 @@
-def getText(string, ns):
-    import re, os
-    import xml.etree.ElementTree as ET
+def buildParentMap():
+    tree = ET.parse("1609_example.xml")
+    parent_map = {c: p for p in tree.iter() for c in p}
+    return parent_map, tree
 
+def find_common_ancestor(tree, element1, element2, ns, mapParent):
+    el1 = tree.find(f'.//TEI:anchor[@xml:id="{element1}"]', ns)
+    el2 = tree.find(f'.//TEI:anchor[@xml:id="{element2}"]', ns)
+    if mapParent[el1] == mapParent[el2]:
+        return mapParent[el1]
+    else:
+        return _find_common_ancestor(mapParent[el1], mapParent[el2], mapParent)
+        
+
+def _find_common_ancestor(element1, element2, mapParent):
+    if mapParent[element1] == mapParent[element2]:
+        return mapParent[element1]
+    else:
+        return _find_common_ancestor(mapParent[element1], mapParent[element2], mapParent)
+
+def getText(string, ns, mapParent, tree):
     # get two targets
     start_end = string.split()
-
-    # get file name
-    fileName = start_end[0].split('#')[0]
 
     # isolate xml:id and build full anchor
     for i, id in enumerate(start_end):
         # start_end[i] = f'<anchor xml:id=\"{id.split("#")[1]}\"/>'
         start_end[i] = id.split("#")[1]
 
-    tree = ET.parse(fileName)
-
-    parent = tree.find(f'.//TEI:anchor[@xml:id="{start_end[0]}"]/..', ns)
+    # Find the earliest common ancestor
+    parent = find_common_ancestor(tree, start_end[0], start_end[1], ns, mapParent)
     collect = False
     tags = []
+    
+    # Go through every element in the ancestor and figure out what elements need to be collected
     for el in parent.iter():
         if el.get(f'\u007b{ns["xml"]}\u007did') == start_end[0]:
             collect = True
@@ -26,27 +41,20 @@ def getText(string, ns):
         if collect:
             tags.append(el)
     text = ""
+
+    # Go through every collected element and collect text
     for tag in tags:
+        # Text only includes inner text until the next tag (which is convienient for me). See:
+        # https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.Element.text
         if tag.text:
             text += ''.join([tag.text, tag.tail])
+            # find a way to preserve inner elements
+            # text += ''.join([f'<{tag.tag}>', tag.text, f'</{tag.tag}>', tag.tail])
         else:
             text += tag.tail
-    print(text)
-    print ("+++++++++++++")
 
         
-    # open file
-    # with open(fileName, 'r', encoding='utf-8') as file:
-    #     text = file.read()
-        # NEED TO REMOVE ANY POSSIBLE EMPTY SPACES BETWEEN THE END OF THE XML:ID AND CLOSING THE TAG
-        # ALTERNATIVELY FIND A WAY OF EXTRACTING TEXT BEWEEN TWO TAGS
-        # find substring
-        # result = re.search(f'{start_end[0]}(.*){start_end[1]}', text).group(1)
-        # print(result)
-
-
-
-    return 'NOTHING'
+    return text
     
 
 import xml.etree.ElementTree as ET
@@ -65,6 +73,9 @@ root = tree.getroot()
 # selects the parent (..) of all ptr inside rdg (i.e., selects rdg)
 rdgs = tree.findall(".//TEI:rdg/TEI:ptr/..", ns)
 
+# Built parent map and tree for 1609
+mapParent1609, tree1609 = buildParentMap()
+
 for rdg in rdgs:
     # Find target
     ptr = rdg.find('TEI:ptr', ns)
@@ -74,7 +85,7 @@ for rdg in rdgs:
     rdg.attrib['source'] = target
 
     # Get text from 1609
-    newText = getText(target, ns)
+    newText = getText(target, ns, mapParent1609, tree1609)
 
     # Replace ptr with text from 1609
     rdg.remove(ptr)
