6590bcf72aff7f5bf3cd57df7415bdcd8ae5dfb8
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-07-10 13:50:36 +0100

    updated csv to only include references to summary notes (but include self references as well), and add preface to the list of chapters

--- scripts/generate_list_nodes.py
+++ scripts/generate_list_nodes.py
@@ -17,6 +17,11 @@ def main():
         # find all divs type chapter
         chapters = root.findall(".//TEI:div[@type='chapter']", namespaces=ns)
 
+        # add div with id="preface" to the list of chapters
+        preface = root.find(".//TEI:div[@id='preface']", namespaces=ns)
+        if preface is not None:
+            chapters.insert(0, preface)
+
         # for each chapter, get a list of all the refs that have a target element
 
         for chapter in chapters:
@@ -30,15 +35,18 @@ def main():
                 target = ref.get("target")
                 source = f'ch{chapter.get("n")}'
                 if target:
-                    destination = target.split("#")[-1]
-                    # extracts chapter number from destination which will be the start of the string until the first non-digit character following the ch
-                    if destination.startswith("ch") and len(destination) > 3:
-                        
-                        if destination[3].isdigit():
-                            destination = destination[:4]
-                        else:
-                            destination = destination[:3]
-                    if source != destination:
+                    # if target contains 'sumn'
+                    if "sumn" in target:
+                        destination = target.split("#")[-1]
+                        # extracts chapter number from destination which will be the start of the string until the first non-digit character following the ch
+                        if destination.startswith("ch") and len(destination) > 3:
+                            
+                            if destination[3].isdigit():
+                                destination = destination[:4]
+                            else:
+                                destination = destination[:3]
+                    
+                    
                         # write the results to an output csv file
                         with open("data-analysis/cross-refs.csv", "a", encoding="utf-8") as out_file:
                             out_file.write(f'{source},{target},{destination}\n')
