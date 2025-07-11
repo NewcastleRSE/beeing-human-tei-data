8aa269c15c1971f2e979f584c5da790214d089cd
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-07-11 09:03:49 +0100

    changse the script so that it also analyses 1609

--- scripts/generate_list_nodes.py
+++ scripts/generate_list_nodes.py
@@ -1,62 +1,50 @@
 import xml.etree.ElementTree as ET
 
-def main():
-    # Define namespaces dictionary to navigate the tree
+def process_file(xml_filename, output_csv, edition_label):
     ns = {
         'TEI': 'http://www.tei-c.org/ns/1.0',
         'xml': 'http://www.w3.org/XML/1998/namespace'
     }
 
-    with open("data-analysis/cross-refs.csv", "w", encoding="utf-8") as out_file:
+    with open(output_csv, "w", encoding="utf-8") as out_file:
         out_file.write("source,target,destination\n")
 
-    with open("1623.xml", "r", encoding="utf-8") as f:
+    with open(xml_filename, "r", encoding="utf-8") as f:
         xml_data = f.read()
         root = ET.fromstring(xml_data)
 
-        # find all divs type chapter
         chapters = root.findall(".//TEI:div[@type='chapter']", namespaces=ns)
-
-        # add div with id="preface" to the list of chapters
         preface = root.find(".//TEI:div[@type='preface']", namespaces=ns)
         if preface is not None:
             chapters.insert(0, preface)
 
-        # for each chapter, get a list of all the refs that have a target element
-
         for chapter in chapters:
             refs = chapter.findall(".//TEI:ref[@target]", namespaces=ns)
-
-            # exclude all the refs whose target does not start with #
             refs = [ref for ref in refs if ref.get("target", "").startswith("#")]
 
-            # print all refs
             for ref in refs:
                 target = ref.get("target")
                 if chapter.get("n") is None and chapter.get('type') == 'preface':
-                    source = 'preface';
+                    source = 'preface'
                 else:
                     source = f'ch{chapter.get("n")}'
                 if target:
-                    # if target contains 'sumn'
                     if "sumn" in target:
                         destination = target.split("#")[-1]
-                        # extracts chapter number from destination which will be the start of the string until the first non-digit character following the ch
                         if destination.startswith("ch") and len(destination) > 3:
-                            
                             if destination[3].isdigit():
                                 destination = destination[:4]
                             else:
                                 destination = destination[:3]
-                    
-                    
-                        # write the results to an output csv file
-                        with open("data-analysis/cross-refs.csv", "a", encoding="utf-8") as out_file:
+                        with open(output_csv, "a", encoding="utf-8") as out_file:
                             out_file.write(f'{source},{target},{destination}\n')
-                        
             if len(refs) == 0:
-                print("No refs found with target attribute.")
-            print(f'{source}: {len(refs)} refs found with target attribute.')
+                print(f"[{edition_label}] No refs found with target attribute.")
+            print(f'[{edition_label}] {source}: {len(refs)} refs found with target attribute.')
+
+def main():
+    process_file("1623.xml", "data-analysis/cross-refs-1623.csv", "1623")
+    process_file("1609.xml", "data-analysis/cross-refs-1609.csv", "1609")
 
 if __name__ == "__main__":
     main()
