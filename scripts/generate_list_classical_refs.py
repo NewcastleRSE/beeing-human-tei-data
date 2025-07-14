6a2659b4c03405961b499182333e191ed9ae8e4b
tiagosousagarcia <tiagosousagarcia@gmail.com>
2025-07-14 14:50:37 +0100

    remove further errors from csv

--- scripts/generate_list_classical_refs.py
+++ scripts/generate_list_classical_refs.py
@@ -54,6 +54,9 @@ def process_file(xml_filename, output_csv):
             # if the author is in the abbreviation dictionary, replace it
             if author in abbr:
                 author = abbr[author]
+            # if the last character is a puncuation mark or a whitespace, remove it
+            if author[-1] in [".", ",", ":", ";", "!", "?", " "]:
+                author = author[:-1]
             # write the author to the CSV file
             with open(output_csv, "a", encoding="utf-8") as out_file:
                 out_file.write(f'butler,{author}\n')
